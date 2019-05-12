9c37de539f7fd3e1b42383d9804ca8194a6eb23b
NickNaso <nicoladelgobbo@gmail.com>
2019-05-12 23:30:49 +0200

    Update winston usage for version 3

--- express-winston-logger/logger/lib/logger.js
+++ express-winston-logger/logger/lib/logger.js
@@ -18,6 +18,8 @@
 
 'use strict'
 
+// path.join('./logs', 'express-winston' + '-ERROR'),
+
 /*!
  * Module dependencies
  */
@@ -27,57 +29,68 @@ require('winston-daily-rotate-file')
 const moment = require('moment')
 const fs = require('fs-extra')
 
-let logger = null
 
-module.exports = Logger;
+const DEFAULT_FORMAT_DATE = 'DD-MM-YYYY - HH:mm:ss'
+const DEFAULT_DATE_PATTERN = 'DD-MM-YYYY'
+const DEFAULT_CONSOLE_LOGGER = true
+const DEFAULT_FILE_LOGGER = false
+const LABEL = 'MY-SERVICE'
 
-function Logger() {
-  if (logger instanceof Logger) {
-    return logger
-  }
-  function timeing() {
-    return '### ' + moment().format('DD-MM-YYYY - HH:mm:ss') + ' ###'
-  }
-  const transports = []
-  // ... do some stuff based on your configuration for example choice which the
-  // winston tranport to use.
-  const optsConsoleTransport = {
-    colorize: true,
-    timestamp: timeing
-  }
-  // Ensure that directory for logs exist otherwise create it
-  fs.ensureDirSync('./logs')
-  const consoleTransport = new (winston.transports.Console)(optsConsoleTransport)
-  transports.push(consoleTransport)
-  const optsFileTransportForError = {
-    name: 'error-file-stransport',
-    filename: path.join('./logs', 'express-winston' + '-ERROR'),
-    level: 'error',
-    json: false,
-    timestamp: timeing,
-    datePattern: '-dd-MM-yyyy.log'
-  }
-  const fileTransportForError = new (winston.transports.DailyRotateFile)(optsFileTransportForError)
-  transports.push(fileTransportForError)
-  const optsFileTransportForWarning = {
-    name: 'warning-file-transport',
-    filename: path.join('./logs', 'express-winston' + '-WARNING'),
-    level: 'warn',
-    json: false,
-    timestamp: timeing,
-    datePattern: '-dd-MM-yyyy.log'
-  }
-  const fileTransportForWarning = new (winston.transports.DailyRotateFile)(optsFileTransportForWarning)
-  transports.push(fileTransportForWarning)
-  const optsFileTransportForInfo = {
-    name: 'info-file-transport',
-    filename: path.join('./logs', 'express-winston' + '-INFO'),
-    level: 'info',
-    json: false,
-    timestamp: timeing,
-    datePattern: '-dd-MM-yyyy.log'
-  }
-  const fileTransportForInfo = new (winston.transports.DailyRotateFile)(optsFileTransportForInfo)
-  transports.push(fileTransportForInfo)
-  return new (winston.Logger)({transports: transports})
+const LEVELS = {
+    ERROR: 'error',
+    WARNING: 'warning',
+    INFO: 'info'
 }
+
+function checkOptions(opts = {}) {
+  let { formatDate, datePattern } = opts
+  formatDate = formatDate ? formatDate : DEFAULT_FORMAT_DATE
+  datePattern = datePattern ? datePattern : DEFAULT_DATE_PATTERN
+  return {
+      formatDate,
+      datePattern
+  }
+}
+
+function buildForConsole(opts) {
+  return winston.createLogger({
+      format: winston.format.combine(
+          winston.format.label({ label: LABEL }),
+          winston.format.colorize(),
+          winston.format.timestamp(),
+          opts.fmt
+      ),
+      transports: new winston.transports.Console()
+  })
+}
+
+function printer(formatDate) {
+  return winston.format.printf(info => {
+      return `${info.label} ## ${moment(info.timestamp).format(formatDate)} ## ${info.level}: ${info.message}`
+  })
+}
+
+class Logger {
+
+  constructor (opts = {}) {
+    const buildOpts = checkOptions(opts)
+    // Function to print and serialize the logs for the chosen transport
+    buildOpts.fmt = printer(buildOpts.formatDate)
+    this.logger = buildForConsole(buildOpts)
+  }
+  
+  error(message) {
+    this.logger.error(message)
+  }
+
+  warn(message) {
+      this.logger.warn(message)
+  }
+
+  info(message) {
+      this.logger.info(message)
+  }
+
+}
+
+module.exports = Logger
